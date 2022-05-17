ebbdd7f1371aae70d69ad83252c84bb446c2a892
Mihail <mishkagoo@gmail.com>
2022-05-17 21:44:42 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import video from './video/vid1.mp4'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay loop muted>
+        <source src={video} type='video/mp4' />
+      </video>
     </div>
   );
 }
